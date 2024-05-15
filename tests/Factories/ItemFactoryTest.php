3f1fef273bf2ac61c7c72df3a7ae3c2c5d4e0ff0
B.Pavel <kiliman6aro@gmail.com>
2024-05-15 13:26:43 +0300

    Add tests coverage ItemFactory

--- tests/Factories/ItemFactoryTest.php
+++ tests/Factories/ItemFactoryTest.php
@@ -5,6 +5,7 @@ use HopHey\Rbac\Entities\Permission;
 use HopHey\Rbac\Entities\Role;
 use HopHey\Rbac\Factories\ItemFactory;
 use PHPUnit\Framework\TestCase;
+use HopHey\Rbac\Entities\Item;
 
 class ItemFactoryTest extends TestCase
 {
@@ -26,6 +27,7 @@ class ItemFactoryTest extends TestCase
         $role = $this->factory->createRole("admin");
         $this->assertInstanceOf(Role::class, $role);
         $this->assertEquals("admin", $role->getName());
+        $this->assertEquals(Item::TYPE_ROLE, $role->getType());
     }
     
     public function testCreatePermission() 
@@ -33,6 +35,31 @@ class ItemFactoryTest extends TestCase
         $permission = $this->factory->createPermission("createPost");
         $this->assertInstanceOf(Permission::class, $permission);
         $this->assertEquals("createPost", $permission->getName());
+        $this->assertEquals(Item::TYPE_PERMISSION, $permission->getType());
+    }
+    
+    public function testCreateRoleByData()
+    {
+        $role = $this->factory->createItemByData(['id' => 12345, 'name' => 'admin', 'type' => 1]);
+        $this->assertInstanceOf(Role::class, $role);
+        $this->assertEquals("admin", $role->getName());
+        $this->assertEquals(12345, $role->getId());
+        $this->assertEquals(Item::TYPE_ROLE, $role->getType());
+    }
+    
+    public function testCreatePermissionByData()
+    {
+        $permission = $this->factory->createItemByData(['id' => 1, 'name' => 'createPost', 'type' => 2]);
+        $this->assertInstanceOf(Permission::class, $permission);
+        $this->assertEquals("createPost", $permission->getName());
+        $this->assertEquals(1, $permission->getId());
+        $this->assertEquals(Item::TYPE_PERMISSION, $permission->getType());
+    }
+    
+    public function testCreateItemWitUnknownType()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->factory->createItemByData(['id' => 12345, 'name' => 'role', 'type' => -1]);
     }
 }
 
